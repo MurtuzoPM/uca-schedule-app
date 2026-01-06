d4fc8b5cecaca05aea0e3c3b9504f923c8c94a87
Murtuzo <murtuzo.mamadziyoev_2028@ucentralasia.org>
2026-01-06 17:24:08 +0500

    Create CorsConfig

---
+++ backend/src/main/java/com/uca/scheduleapp/config/CorsConfig.java
@@ -0,0 +1,22 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Разрешаем все пути
+                        .allowedOrigins("http://138.197.192.172:3000") // Твой фронтенд
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
