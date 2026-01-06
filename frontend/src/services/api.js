9a930b48bf088f3ebf73917f7a135db390eb3750
Murtuzo <murtuzo.mamadziyoev_2028@ucentralasia.org>
2026-01-06 17:10:40 +0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// THIS LINE MUST STAY LIKE THIS
-const API_URL = '/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 
 const api = axios.create({
     baseURL: API_URL,
